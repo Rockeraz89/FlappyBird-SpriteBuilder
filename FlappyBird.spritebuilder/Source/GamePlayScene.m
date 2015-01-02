58fa81da4bd3547899998de6f0047d9dc5bc60bf
Shaun Bolton <ShaunsMac@Shauns-MacBook-Pro.local>
2015-01-02 21:38:38 +0000

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -14,19 +14,19 @@
 }
 
 -(void)update:(CCTime)delta
-// Increment the time since the last obstacle was added
-timeSinceObstacle: += delta; // delta is approximately 1/60th of a second
-
-// Check to see if two seconds have passed
-if (timeSinceObstacle > 2.0f)
 {
-    // Add a new obstacle
-    [self addObstacle];
+    // Increment the time since the last obstacle was added
+    timeSinceObstacle += delta; // delta is approximately 1/60th of a second
     
-    // Then reset the timer.
-    timeSinceObstacle = 0.0f;
-}
-{
+    // Check to see if two seconds have passed
+    if (timeSinceObstacle > 2.0f)
+    {
+        // Add a new obstacle
+        [self addObstacle];
+        
+        // Then reset the timer.
+        timeSinceObstacle = 0.0f;
+    }
     // put update code here
 }
 
